8bebf21f805ebaa1508a815157002ef00b88cf19
Satyarth Prakash Yadav <84712739+satyarth7619@users.noreply.github.com>
2024-06-09 01:34:20 +0530

    Gym Website

--- fitnessLife/fitnessLife/settings.py
+++ fitnessLife/fitnessLife/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,8 +125,15 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')  # Remove the extra slash here
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
