da0e41ea223bfe9718707199187b47850002e6fe
Satyarth Prakash Yadav <84712739+satyarth7619@users.noreply.github.com>
2024-06-20 23:26:29 +0530

    Update settings.py

--- fitnessLife/fitnessLife/settings.py
+++ fitnessLife/fitnessLife/settings.py
@@ -83,7 +83,16 @@ WSGI_APPLICATION = 'fitnessLife.wsgi.application'
 #    }
 #}
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'myrdshost.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
