eb763755e65733d967e5afc4616ea8967be07f70
Satyarth Prakash Yadav <84712739+satyarth7619@users.noreply.github.com>
2024-06-20 23:20:19 +0530

    Update settings.py

--- fitnessLife/fitnessLife/settings.py
+++ fitnessLife/fitnessLife/settings.py
@@ -117,15 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'  # Note the leading slash is important
-
 import os
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')  # Remove the extra slash here
 ]
-
+STATICFILES_DIRS = [BASE_DIR/'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
