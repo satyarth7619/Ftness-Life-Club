b004c194a175420ce9015312ff7383e876a88ad9
Satyarth Prakash Yadav <84712739+satyarth7619@users.noreply.github.com>
2024-06-29 03:12:51 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,8 @@ echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-echo "Collect Static..."
+
 python3.9 manage.py collectstatic --noinput --clear
