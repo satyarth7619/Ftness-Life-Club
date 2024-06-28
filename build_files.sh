1eb28cc95787bb06a8f96373c8dd8df768187a11
Satyarth Prakash Yadav <84712739+satyarth7619@users.noreply.github.com>
2024-06-29 02:24:40 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files and run migrations
 python3.9 manage.py collectstatic --noinput
+python3.9 manage.py migrate
